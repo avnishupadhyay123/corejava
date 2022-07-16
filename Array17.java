a8e05a253bec0f43305ee2202e7c45e45d5a4f76
avnishupadhyay123 <uavnish6@gmail.com>
2022-07-16 13:15:32 +0530

    sucess

---
+++ Array17.java
@@ -0,0 +1,31 @@
+package com.rays.basics;
+
+
+
+public class Array17 {  
+	public static void main(String[] args) {
+		int [] a = {456994,876,987789};             
+	    int b =a[0]; 
+	    for (int i = 0; i < a.length; i++) {
+		if (a[i]>b) {
+			
+			b=a[i];
+			
+			
+		}	
+		}
+
+	System.out.println(b);
+
+	
+	String s="ram";
+	
+System.out.println(s.charAt(1));	
+System.out.println(s.length());
+System.out.println(s.toUpperCase());
+
+
+
+	}
+	}
+
